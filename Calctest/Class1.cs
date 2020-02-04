e755512aaefb26ac497b9bb4210042347bf6de6f
SBrahe <201807173@post.au.dk>
2020-02-04 08:49:23 +0100

    Tilføjet testen

---
+++ Calctest/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorTestUnit
+    {
+        private CalculatorClass _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Calculator.CalculatorClass();
+        }
+
+        [Test]
+        public void Add_OnePlusOne_returnsTwo()
+        {
+            //Arrange- setup
+
+            //Act 
+
+            //Assert
+
+            Assert.That(_uut.Add(1, 1), Is.EqualTo(2));
+        }
+
+        [Test]
+
+        public void Subtract_OneFromTwo_ReturnsOne()
+        {
+            Assert.That(_uut.Subtract(2, 1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Multiply_TwoTimesTwo_ReturnsFour()
+        {
+            Assert.That(_uut.Multiply(2, 2), Is.EqualTo(4));
+        }
+
+        [Test]
+
+        public void Power_TwoToThePowerOfFour_ReturnsSixteen()
+        {
+            Assert.That(_uut.Power(2, 4), Is.EqualTo(16));
+        }
+    }
+}
